ca798326584c3be66c5808f7b0217a35c05ebda7
Jakob Ladegaard Møller <jlm1999@live.dk>
2017-08-08 12:03:18 +0200

    Added Source to BasicTextEditor.cs, and removed an object from MainWindow.xaml

--- BasicTextEditor/BasicTextEditorDev/BasicTextEditor.cs
+++ BasicTextEditor/BasicTextEditorDev/BasicTextEditor.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BasicTextEditorDev
 {
     public class BasicTextEditor : TextBox
     {
-        public BasicTextEditor()
+        public TextBox Source
         {
-
+            get => (GetValue(SourceProperty) as TextBox); set => SetValue(SourceProperty, value);
         }
+        public static readonly DependencyProperty SourceProperty =
+          DependencyProperty.Register("Source", typeof(TextBox), typeof(BasicTextEditor));
 
         //TODO: eventhandlers til flg.
         /*

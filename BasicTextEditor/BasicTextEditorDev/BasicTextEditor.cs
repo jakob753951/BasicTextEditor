b5c2f97c45cbca31a228cd7b2f6aac3db6ef4b2e
Jakob Ladegaard Møller <jlm1999@live.dk>
2017-08-09 14:36:16 +0200

    Moved ContextMenu to cs code

--- BasicTextEditor/BasicTextEditorDev/BasicTextEditor.cs
+++ BasicTextEditor/BasicTextEditorDev/BasicTextEditor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,7 +10,29 @@ namespace BasicTextEditorDev
     {
         public BasicTextEditor()
         {
+            string[] menuItems = new string[8] { "Bold", "Italic", null, "Increase font size", "Decrease font size", null, "Change colour", "Create link" };
             DataContext = this;
+            MenuItem[] items = new MenuItem[8];
+            for(int i = 0; i < items.Length; i++)
+            {
+                items[i] = new MenuItem();
+            }
+            ContextMenu cm = new ContextMenu();
+            MenuItem miBold = new MenuItem(), miItalics = new MenuItem(), miFontIncr = new MenuItem(), miFontDecr = new MenuItem(), miColour = new MenuItem(), miLink = new MenuItem();
+
+            for(int i = 0; i < menuItems.Length; i++)
+            {
+                if(menuItems[i] != null)
+                {
+                    items[i].Header = menuItems[i];
+                    cm.Items.Add(items[i]);
+                }
+                else
+                {
+                    cm.Items.Add(new Separator());
+                }
+            }
+            this.ContextMenu = cm;
         }
 
         public RichTextBox Source
